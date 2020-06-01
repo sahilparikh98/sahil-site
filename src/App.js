6442b3f0b1967c3b6a25319700fc3809f0bc7ea4
sahilparikh98 <sparikh98@gmail.com>
2020-05-31 20:00:03 -0500

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav, Container} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Navbar bg="clear" expand="lg">
+        <Navbar.Brand href="#home">sahil parikh</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">home</Nav.Link>
+            <Nav.Link href="#link">about</Nav.Link>
+            <Nav.Link href="#link">projects</Nav.Link>
+            <Nav.Link href="#link">creative</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="main-content">
+        <h1>hi! i'm sahil parikh. i'm currently an engineer at yext.</h1>
+      </div>
+    </Container>
   );
 }
 
