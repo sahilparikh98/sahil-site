c959543ae6d827a9f01dd00fcefd0a19747fdf17
sahilparikh98 <sparikh98@gmail.com>
2020-06-03 17:54:01 -0500

    add and implement react document title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import DocumentTitle from 'react-document-title'
 
 function App() {
   return (
-    <Container fluid>
-      <Navbar bg="clear" expand="lg">
-        <Navbar.Brand href="#home">sahil parikh</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">home</Nav.Link>
-            <Nav.Link href="#link">about</Nav.Link>
-            <Nav.Link href="#link">projects</Nav.Link>
-            <Nav.Link href="#link">creative</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <div className="main-content">
-        <h1>hi! i'm sahil parikh. i'm currently an engineer at yext.</h1>
-      </div>
-    </Container>
+    <DocumentTitle title="sahil parikh">
+      <Container fluid>
+        <Navbar bg="clear" expand="lg">
+          <Navbar.Brand href="#home">sahil parikh</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">home</Nav.Link>
+              <Nav.Link href="#link">about</Nav.Link>
+              <Nav.Link href="#link">projects</Nav.Link>
+              <Nav.Link href="#link">creative</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="main-content">
+          <h1>hi! i'm sahil parikh. i'm currently an engineer at yext.</h1>
+        </div>
+      </Container>
+    </DocumentTitle>
   );
 }
 
