74fb20b0a74e47528524cfc63ec82ec3110dcaf4
sahilparikh98 <sparikh98@gmail.com>
2020-06-04 20:18:32 -0500

    add spacing between work experiences

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function WorkExperience(props){
   return (
-    <div>
+    <div className="row-spacing">
       <h5><b>{props.position} @ {props.company} ({props.year})</b></h5>
       <h6> - {props.team}</h6>
     </div>
