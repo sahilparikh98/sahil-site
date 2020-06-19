0d752bada1a9d6d2e15fb369665d56ef9e9bb0f4
sahilparikh98 <sparikh98@gmail.com>
2020-06-19 16:55:22 -0500

    delete old pill file

--- src/components/Pill.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Badge } from 'react-bootstrap'
-
-export default function Pill(props) {
-  return (
-      <Badge className={props.children} variant="dark">{props.children}</Badge>
-  );
-}
