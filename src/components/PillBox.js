b7fbffb92dacc5bd34c953df3c6d18f53bbaa884
sahilparikh98 <sparikh98@gmail.com>
2020-06-19 16:55:06 -0500

    change paradigm for creating pills

---
+++ src/components/PillBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Badge } from 'react-bootstrap'
+
+export default class PillBox extends React.Component {
+
+  generatePills() {
+    return this.props.names.map((name) => {
+      return <Badge className={name} variant="dark">{name}</Badge>
+    })
+  }
+
+  render() {
+    return this.generatePills();
+  }
+}
