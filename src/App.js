4cf01a325e830660bcbe367b0eedb2b5e4a557c3
sahilparikh98 <sparikh98@gmail.com>
2020-06-24 19:24:43 -0500

    add text color changing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Projects from "./views/Projects";
 import Creative from "./views/Creative";
 import BlogPost from "./views/BlogPost";
 import Typing from "react-typing-animation";
-import ColorWrapper from './components/ColorWrapper';
+import ColorWrapper from "./components/ColorWrapper";
 
 class App extends React.Component {
   constructor(props) {
@@ -21,65 +21,84 @@ class App extends React.Component {
     return (
       <DocumentTitle title="sahil parikh">
         <ColorWrapper>
-        <Container fluid>
-          <Router>
-            <Navbar bg="clear" expand="lg">
-              <IndexLinkContainer to="/">
-                <Navbar.Brand href="/">sahil parikh</Navbar.Brand>
-              </IndexLinkContainer>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                  <LinkContainer to="/">
-                    <Nav.Link href="/">home</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/about">
-                    <Nav.Link href="/about">about</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/projects">
-                    <Nav.Link href="/projects">projects</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/creative">
-                    <Nav.Link href="/creative">creative</Nav.Link>
-                  </LinkContainer>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-            <Switch>
-              <Route exact path="/">
-                <div className="home-content">
-                  <Typing speed={40}>
-                    <h1>
-                      hi! i'm sahil parikh. i'm a software engineer at{" "}
-                      <a href="https://www.yext.com" className="blog-list-link">
-                        yext
-                      </a>
-                      .
-                    </h1>
-                    <h1>
-                      i'm currently based in new york city.{" "}
-                      <span role="img" aria-label="current-time-emoji">
-                        🌇
-                      </span>
-                    </h1>
-                  </Typing>
-                </div>
-              </Route>
-              <Route exact path="/about">
-                <About />
-              </Route>
-              <Route exact path="/projects">
-                <Projects />
-              </Route>
-              <Route exact path="/creative">
-                <Creative />
-              </Route>
-              <Route exact path="/creative/:blogId" component={BlogPost} />
-            </Switch>
-          </Router>
-        </Container>
+          <Container fluid>
+            <Router>
+              <Navbar bg="clear" expand="sm" collapseOnSelect={true}>
+                <IndexLinkContainer
+                  to="/"
+                  activeClassName="nav-brand-active-color"
+                >
+                  <Navbar.Brand href="/" className="nav-brand-active-color">
+                    sahil parikh
+                  </Navbar.Brand>
+                </IndexLinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="ml-auto">
+                    <IndexLinkContainer
+                      to="/"
+                      activeClassName="nav-link-active-color"
+                    >
+                      <Nav.Link href="/">home</Nav.Link>
+                    </IndexLinkContainer>
+                    <LinkContainer
+                      to="/about"
+                      activeClassName="nav-link-active-color"
+                    >
+                      <Nav.Link href="/about">about</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer
+                      to="/projects"
+                      activeClassName="nav-link-active-color"
+                    >
+                      <Nav.Link href="/projects">projects</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer
+                      to="/creative"
+                      activeClassName="nav-link-active-color"
+                    >
+                      <Nav.Link href="/creative">creative</Nav.Link>
+                    </LinkContainer>
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+              <Switch>
+                <Route exact path="/">
+                  <div className="home-content">
+                    <Typing speed={40}>
+                      <h1>
+                        hi! i'm sahil parikh. i'm a software engineer at{" "}
+                        <a
+                          href="https://www.yext.com"
+                          className="blog-list-link"
+                        >
+                          yext
+                        </a>
+                        .
+                      </h1>
+                      <h1>
+                        i'm currently based in new york city.{" "}
+                        <span role="img" aria-label="current-time-emoji">
+                          🌇
+                        </span>
+                      </h1>
+                    </Typing>
+                  </div>
+                </Route>
+                <Route exact path="/about">
+                  <About />
+                </Route>
+                <Route exact path="/projects">
+                  <Projects />
+                </Route>
+                <Route exact path="/creative">
+                  <Creative />
+                </Route>
+                <Route exact path="/creative/:blogId" component={BlogPost} />
+              </Switch>
+            </Router>
+          </Container>
         </ColorWrapper>
-        
       </DocumentTitle>
     );
   }
--- src/components/ColorWrapper.js
+++ src/components/ColorWrapper.js
@@ -1,38 +1,89 @@
-import React from 'react';
-import { TwitterPicker } from 'react-color';
-import reactCSS from 'reactcss';
+import React from "react";
+import { CirclePicker } from "react-color";
+import { Popover, OverlayTrigger, Button } from "react-bootstrap";
+import reactCSS from "reactcss";
+
+const defaultColors = {
+  black: {
+    rgb: {
+      r: "0",
+      g: "0",
+      b: "0",
+      a: "100",
+    },
+  },
+  white: {
+    rgb: {
+      r: "255",
+      g: "255",
+      b: "255",
+      a: "100",
+    },
+  },
+};
 
 export default class ColorWrapper extends React.Component {
   constructor(props) {
     super(props);
-    console.log("constructor for color wrapper")
     this.state = {
       displayColorPicker: false,
       color: {
-        r: '0',
-        g: '0',
-        b: '0',
-        a: '0',
+        r: "0",
+        g: "0",
+        b: "0",
+        a: "0",
       },
+      textColor: {
+        r: "0",
+        g: "0",
+        b: "0",
+        a: "100",
+      },
+      darkTheme: false,
     };
   }
 
   handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+    this.setState({ displayColorPicker: !this.state.displayColorPicker });
   };
 
   handleClose = () => {
-    this.setState({ displayColorPicker: false })
+    this.setState({ displayColorPicker: false });
   };
 
   handleChange = (color) => {
-    this.setState({ color: color.rgb })
+    this.setState({ color: color.rgb });
+  };
+
+  handleTextColorChange = (color) => {
+    this.setState({ textColor: color.rgb });
   };
 
   handleChangeComplete = (color, event) => {
-    this.setState({ color: color.rgb, displayColorPicker: !this.state.displayColorPicker }, () => {
-      document.body.style.backgroundColor = `rgba(${ this.state.color.r }, ${ this.state.color.g }, ${ this.state.color.b }, ${ this.state.color.a })`;
+    this.setState(
+      { color: color.rgb, displayColorPicker: !this.state.displayColorPicker },
+      () => {
+        document.body.style.backgroundColor = `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`;
+      }
+    );
+  };
+
+  handleTextColorChangeComplete = (color, event) => {
+    this.setState({
+      textColor: color.rgb,
+      displayColorPicker: !this.state.displayColorPicker,
     });
+  };
+
+  handleDarkThemeClick = () => {
+    if (!this.state.darkTheme) {
+      this.handleChangeComplete(defaultColors.black);
+      this.handleTextColorChangeComplete(defaultColors.white);
+    } else {
+      this.handleChangeComplete(defaultColors.white);
+      this.handleTextColorChangeComplete(defaultColors.black);
+    }
+    this.setState({ darkTheme: !this.state.darkTheme });
   };
 
   shouldComponentUpdate(nextState) {
@@ -41,45 +92,88 @@ export default class ColorWrapper extends React.Component {
 
   render() {
     const styles = reactCSS({
-      'default': {
+      default: {
         height: {
           minHeight: "95vh",
+          color: `rgba(${this.state.textColor.r}, ${this.state.textColor.g}, ${this.state.textColor.b}, ${this.state.textColor.a})`,
+          borderColor: `rgba(${this.state.textColor.r}, ${this.state.textColor.g}, ${this.state.textColor.b}, ${this.state.textColor.a})`,
         },
         swatch: {
-          paddingRight: '20px',
-          border: 'none',
-          background: `rgba(${ this.state.color.r }, ${ this.state.color.g }, ${ this.state.color.b }, ${ this.state.color.a })`,
-          textDecoration: 'underline',
-          cursor: 'pointer',
+          paddingRight: "20px",
+          border: "none",
+          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
+          color: `rgba(${this.state.textColor.r}, ${this.state.textColor.g}, ${this.state.textColor.b}, ${this.state.textColor.a})`,
+          textDecoration: "underline",
+          cursor: "pointer",
         },
         bottom: {
-          fontSize: '20px'
-        },
-        popover: {
-          zIndex: '2',
+          fontSize: "20px",
         },
         cover: {
-          position: 'fixed',
-          top: '0px',
-          right: '0px',
-          bottom: '0px',
-          left: '0px',
+          position: "fixed",
+          top: "0px",
+          right: "0px",
+          bottom: "0px",
+          left: "0px",
+        },
+        clearButton: {
+          border: "none",
+          background: "none",
+          paddingTop: "10px",
+          cursor: "pointer",
+          textDecoration: "underline",
+          color: "black",
         },
       },
     });
+    const Pop = (
+      <Popover id="popover-basic">
+        <Popover.Content>
+          <h6>change background color:</h6>
+          <CirclePicker
+            triangle="hide"
+            color={this.state.color}
+            onChange={this.handleChange}
+            onChangeComplete={this.handleChangeComplete}
+          />
+          <h6 className="main-content">change text color:</h6>
+          <CirclePicker
+            triangle="hide"
+            color={this.state.textColor}
+            onChange={this.handleTextColorChange}
+            onChangeComplete={this.handleTextColorChangeComplete}
+          />
+          {this.state.darkTheme ? (
+            <Button
+              style={styles.clearButton}
+              onClick={this.handleDarkThemeClick}
+            >
+              light theme
+            </Button>
+          ) : (
+            <Button
+              style={styles.clearButton}
+              onClick={this.handleDarkThemeClick}
+            >
+              dark theme
+            </Button>
+          )}
+        </Popover.Content>
+      </Popover>
+    );
     return (
-    <div>
-      <div style={styles.height}>
-        {this.props.children}
-      </div>
-      <div style={styles.bottom}>
-        { this.state.displayColorPicker ? <div style={ styles.popover }>
-            <div onClick={ this.handleClose }/>
-            <TwitterPicker color={ this.state.color } onChange={ this.handleChange } onChangeComplete={this.handleChangeComplete}/>
-          </div> : null }
-          <button style={ styles.swatch } onClick={ this.handleClick }>change background color</button>
+      <div>
+        <div style={styles.height} onClick={this.handleClose}>
+          {this.props.children}
         </div>
-    </div>
+        <div style={styles.bottom}>
+          <OverlayTrigger trigger="click" placement="top" overlay={Pop}>
+            <button style={styles.swatch} onClick={this.handleClick}>
+              change colors
+            </button>
+          </OverlayTrigger>
+        </div>
+      </div>
     );
   }
 }
