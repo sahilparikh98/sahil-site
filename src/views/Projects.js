c85ad0809a6b440ce6c2abd45088bc9355254396
sahilparikh98 <sparikh98@gmail.com>
2020-11-03 19:45:07 -0600

    add notion app to projects

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -5,20 +5,13 @@ import Project from "../components/Project";
 
 const projectData = [
   {
-    name: "bet",
-    type: "iOS app",
-    git: "https://github.com/sahilparikh98/bet",
+    name: "notion at the bar",
+    type: "electron app",
+    git: "https://github.com/sahilparikh98/notion-at-the-bar",
     description:
-      "bet is an iOS app that allows you to create and share bets with friends. this was the first app i ever made courtesy of my friends at make school. it is now unusable",
-    tech: ["swift", "parse"],
-  },
-  {
-    name: "dialogue",
-    type: "iOS app",
-    git: "https://github.com/wlodawskymichael/dialogue",
-    description:
-      "a group messaging app that allows you to spectate conversations. this project was cool since it has real-time messaging using firebase. firebase is really neat and can take care of a lot of things for you.",
-    tech: ["swift", "firebase"],
+      "a simple Electron app to access Notion from the macOS menu bar.",
+    tech: ["electron", "javascript"],
+    image: "electron.png",
   },
   {
     name: "sahil-site",
@@ -27,12 +20,31 @@ const projectData = [
     description:
       "my personal website, built using ReactJS and Bootstrap. it features my blog and other fun information about me.",
     tech: ["react"],
+    image: "react.png",
+  },
+  {
+    name: "dialogue",
+    type: "iOS app",
+    git: "https://github.com/wlodawskymichael/dialogue",
+    description:
+      "a group messaging app that allows you to spectate conversations. this project was cool since it has real-time messaging using firebase. firebase is really neat and can take care of a lot of things for you.",
+    tech: ["swift", "firebase"],
+    image: "dialogue.png",
+  },
+  {
+    name: "bet",
+    type: "iOS app",
+    git: "https://github.com/sahilparikh98/bet",
+    description:
+      "bet is an iOS app that allows you to create and share bets with friends. this was the first app i ever made courtesy of my friends at make school. it is now unusable",
+    tech: ["swift", "parse"],
+    image: "bet.png",
   },
 ];
 
 export default class Projects extends React.Component {
   generateImagePath = (key) => {
-    return "../img/" + key + ".png";
+    return "../img/" + key;
   }
 
   createProjects = () => {
@@ -44,11 +56,7 @@ export default class Projects extends React.Component {
           git={project.git}
           description={project.description}
           tech={project.tech}
-          image={
-            project.type === "react web app"
-              ? this.generateImagePath("react")
-              : this.generateImagePath(project.name)
-          }
+          image={this.generateImagePath(project.image)}
         />
       );
     });
